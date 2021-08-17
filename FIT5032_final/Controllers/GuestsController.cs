94a8b0d2db8a397dabec869bdea7134874f04ae0
hong <873696121@qq.com>
2021-08-17 21:46:52 +0800

    FIT5032 final

---
+++ FIT5032_final/Controllers/GuestsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_final.Models;
+
+namespace FIT5032_final.Controllers
+{
+    public class GuestsController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Guests
+        public ActionResult Index()
+        {
+            var guests = db.Guests.Include(g => g.Room);
+            return View(guests.ToList());
+        }
+
+        // GET: Guests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guest);
+        }
+
+        // GET: Guests/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name");
+            return View();
+        }
+
+        // POST: Guests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,RoomId,Address,StartDate,EndDate")] Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guests.Add(guest);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", guest.RoomId);
+            return View(guest);
+        }
+
+        // GET: Guests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", guest.RoomId);
+            return View(guest);
+        }
+
+        // POST: Guests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,RoomId,Address,StartDate,EndDate")] Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guest).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Name", guest.RoomId);
+            return View(guest);
+        }
+
+        // GET: Guests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guest);
+        }
+
+        // POST: Guests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Guest guest = db.Guests.Find(id);
+            db.Guests.Remove(guest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FIT5032_final/extra/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_final.extra
+{
+    public class EmailSender
+    {
+
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@localhost.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
